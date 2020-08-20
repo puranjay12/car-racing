5f920b6823235896061cf8d712b2d8597f7e2928
puranjay12 <65854124+puranjay12@users.noreply.github.com>
2020-08-20 18:32:54 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -88,7 +88,7 @@ class Game {
 
     }
 
-    if(keyIsDown(UP_ARROW) && player.index !== null){
+    if(keyIsDown(UP_ARROW) && player.index !== null||keyCode === 32&&player.index !== null){
       player.distance +=10
       player.update();
     }
