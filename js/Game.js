a42280290b11102829e4e0c374049b656aefa6d6
puranjay12 <65854124+puranjay12@users.noreply.github.com>
2020-08-20 18:38:08 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -81,8 +81,8 @@ class Game {
           camera.position.y = cars[index-1].y;
         }
        
-        textSize(15);
-        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, camera.position.x+100, camera.position.y)
+       // textSize(15);
+        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, camera.position.x+100, camera.position.y)
         
       }
 
